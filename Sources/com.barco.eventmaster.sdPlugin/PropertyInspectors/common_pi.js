f53f16b40cbdb9d2baf9eb2f38fd4fb2e6a98494
robbie78738 <robbie.bruce@barco.com>
2019-03-15 13:09:29 -0500

    Update from laptop
    update tree

---
+++ Sources/com.barco.eventmaster.sdPlugin/PropertyInspectors/common_pi.js
@@ -0,0 +1,213 @@
+var websocket = null,
+    uuid = null,
+    actionInfo = {},
+    inInfo = {},
+    runningApps = [],
+    isQT = navigator.appVersion.includes('QtWebEngine');
+    
+    
+
+function connectSocket(inPort, inUUID, inRegisterEvent, inInfo, inActionInfo) {
+    uuid = inUUID;
+    actionInfo = JSON.parse(inActionInfo); // cache the info
+    inInfo = JSON.parse(inInfo);
+    websocket = new WebSocket('ws://localhost:' + inPort);
+
+    addDynamicStyles(inInfo.colors);
+
+    websocket.onopen = function () {
+        var json = {
+            event: inRegisterEvent,
+            uuid: inUUID
+        };
+
+        websocket.send(JSON.stringify(json));
+
+        // Notify the plugin that we are connected
+        sendValueToPlugin('propertyInspectorConnected', 'property_inspector');
+    };
+
+    websocket.onmessage = function (evt) {
+        // Received message from Stream Deck
+        var jsonObj = JSON.parse(evt.data);
+
+        // This event should come
+        if (jsonObj.event === 'sendToPropertyInspector') {
+            var payload = jsonObj.payload;
+
+            var ipAddress = document.getElementById('ipAddress');
+            ipAddress.value = payload['ipAddress'];
+
+            var port = document.getElementById('port');
+            port.value = payload['port'];
+
+            var presetName = document.getElementById('presetName');
+            if( presetName != null )
+                presetName.value = payload['presetName'];
+
+            var presetMode = document.getElementById('toPVWRadio');
+            if( presetMode != null ) {
+                if( payload["presetMode"] ) {
+                    if( payload["presetMode"] == 0)
+                        presetMode.checked = true;
+                    else
+                        presetMode.checked = false;
+                }
+            }
+        }
+    };
+}
+
+function updateSettings() {
+    var ipAddress = document.getElementById('ipAddress');
+    var port = document.getElementById('port');
+    var presetName = document.getElementById('presetName');
+    var pvmMode = document.getElementById('toPVWRadio')
+    
+    var payload = {};
+
+    payload.property_inspector = 'updateSettings';
+
+    if( ipAddress != null )
+        payload.ipAddress = ipAddress.value;
+    
+    if( port != null )
+        payload.port = port.value;
+    
+    if( presetName != null)
+        payload.presetName = presetName.value
+
+    if( pvmMode.checked == true ) {
+        payload.presetMode = 0;
+    }
+    else {
+         payload.presetMode = 1;
+    }
+    
+    sendPayloadToPlugin(payload);
+}
+
+function sendPayloadToPlugin(payload) {
+    if (websocket && (websocket.readyState === 1)) {
+        const json = {
+            'action': actionInfo['action'],
+            'event': 'sendToPlugin',
+            'context': uuid,
+            'payload': payload
+        };
+        websocket.send(JSON.stringify(json));
+    }
+}
+
+// our method to pass values to the plugin
+function sendValueToPlugin(value, param) {
+    if (websocket && (websocket.readyState === 1)) {
+        const json = {
+            'action': actionInfo['action'],
+            'event': 'sendToPlugin',
+            'context': uuid,
+            'payload': {
+                [param]: value
+            }
+        };
+        websocket.send(JSON.stringify(json));
+    }
+}
+
+
+if (!isQT) {
+    document.addEventListener('DOMContentLoaded', function () {
+        initPropertyInspector();
+    });
+}
+
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+
+    // Notify the plugin we are about to leave
+    sendValueToPlugin('propertyInspectorWillDisappear', 'property_inspector');
+
+    // Don't set a returnValue to the event, otherwise Chromium with throw an error.
+});
+
+function initPropertyInspector() {
+    // Place to add functions
+}
+
+
+function addDynamicStyles(clrs) {
+    const node = document.getElementById('#sdpi-dynamic-styles') || document.createElement('style');
+    if (!clrs.mouseDownColor) clrs.mouseDownColor = fadeColor(clrs.highlightColor, -100);
+    const clr = clrs.highlightColor.slice(0, 7);
+    const clr1 = fadeColor(clr, 100);
+    const clr2 = fadeColor(clr, 60);
+    const metersActiveColor = fadeColor(clr, -60);
+
+    node.setAttribute('id', 'sdpi-dynamic-styles');
+    node.innerHTML = `
+
+    input[type="radio"]:checked + label span,
+    input[type="checkbox"]:checked + label span {
+        background-color: ${clrs.highlightColor};
+    }
+
+    input[type="radio"]:active:checked + label span,
+    input[type="radio"]:active + label span,
+    input[type="checkbox"]:active:checked + label span,
+    input[type="checkbox"]:active + label span {
+      background-color: ${clrs.mouseDownColor};
+    }
+
+    input[type="radio"]:active + label span,
+    input[type="checkbox"]:active + label span {
+      background-color: ${clrs.buttonPressedBorderColor};
+    }
+
+    td.selected,
+    td.selected:hover,
+    li.selected:hover,
+    li.selected {
+      color: white;
+      background-color: ${clrs.highlightColor};
+    }
+
+    .sdpi-file-label > label:active,
+    .sdpi-file-label.file:active,
+    label.sdpi-file-label:active,
+    label.sdpi-file-info:active,
+    input[type="file"]::-webkit-file-upload-button:active,
+    button:active {
+      background-color: ${clrs.buttonPressedBackgroundColor};
+      color: ${clrs.buttonPressedTextColor};
+      border-color: ${clrs.buttonPressedBorderColor};
+    }
+
+    ::-webkit-progress-value,
+    meter::-webkit-meter-optimum-value {
+        background: linear-gradient(${clr2}, ${clr1} 20%, ${clr} 45%, ${clr} 55%, ${clr2})
+    }
+
+    ::-webkit-progress-value:active,
+    meter::-webkit-meter-optimum-value:active {
+        background: linear-gradient(${clr}, ${clr2} 20%, ${metersActiveColor} 45%, ${metersActiveColor} 55%, ${clr})
+    }
+    `;
+    document.body.appendChild(node);
+};
+
+/** UTILITIES */
+
+/*
+    Quick utility to lighten or darken a color (doesn't take color-drifting, etc. into account)
+    Usage:
+    fadeColor('#061261', 100); // will lighten the color
+    fadeColor('#200867'), -100); // will darken the color
+*/
+function fadeColor(col, amt) {
+    const min = Math.min, max = Math.max;
+    const num = parseInt(col.replace(/#/g, ''), 16);
+    const r = min(255, max((num >> 16) + amt, 0));
+    const g = min(255, max((num & 0x0000FF) + amt, 0));
+    const b = min(255, max(((num >> 8) & 0x00FF) + amt, 0));
+    return '#' + (g | (b << 8) | (r << 16)).toString(16).padStart(6, 0);
+}
--- Sources/com.barco.eventmaster.sdPlugin/app.js
+++ Sources/com.barco.eventmaster.sdPlugin/app.js
@@ -4,7 +4,6 @@ var pluginUUID = null;
 var settingsCache = {};
 var jrpc = null;
 
-var deleteThisVar=null;
        
 var DestinationEnum = Object.freeze({ "HARDWARE_AND_SOFTWARE": 0, "HARDWARE_ONLY": 1, "SOFTWARE_ONLY": 2 })
 
