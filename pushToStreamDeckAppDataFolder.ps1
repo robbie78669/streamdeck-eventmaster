93c13b33ded915faccb166ba19e4a3ddb65fab3e
robbie78669 <robbiecbruce@gmail.com>
2020-08-16 16:03:05 -0500

    Update pushToStreamDeckAppDataFolder.ps1

--- pushToStreamDeckAppDataFolder.ps1
+++ pushToStreamDeckAppDataFolder.ps1
@@ -1,1 +1,1 @@
-Copy-Item  "Sources\com.barco.eventmaster.sdPlugin" -Destination "C:\Users\robru\AppData\Roaming\Elgato\StreamDeck\Plugins\" -Recurse -Force
+Copy-Item  "Sources\com.barco.eventmaster.sdPlugin" -Destination "C:\Users\robbi\AppData\Roaming\Elgato\StreamDeck\Plugins\" -Recurse -Force
